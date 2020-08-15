27fb30bc07ab83341d07d2117a7ddd29c05187ec
ARMIN <armingodarzi1380@gmail.com>
2020-08-15 18:13:30 +0430

    add method for handeling erors

--- 5_upload_file/main.go
+++ 5_upload_file/main.go
@@ -34,32 +34,26 @@ func handle(w http.ResponseWriter, req *http.Request) {
 		s := req.FormValue("submitation") == "on"
 		f, h, err := req.FormFile("fname")
 		defer f.Close()
-		if err != nil {
-			log.Fatalln(err)
-		}
+		handleErr(err)
 		fmt.Println(f, h)
 		reader, err2 := ioutil.ReadAll(f)
-		if err2 != nil {
-			log.Fatalln(err2)
-		}
+		handleErr(err2)
 		d = data{n, l, string(reader), s}
 		if s {
 			nf, er := os.Create(filepath.Join("./newFiles/", h.Filename+"2"))
-			if er != nil {
-				log.Fatalln(er)
-			}
+			handleErr(er)
 			defer nf.Close()
 			_, errr := nf.WriteString(string(reader))
-			if errr != nil {
-				log.Fatalln(errr)
-			}
+			handleErr(errr)
 		}
 	} else {
 		d = data{"", "", "", false}
-		fmt.Println("FUCK ME")
 	}
 	err3 := tpl.ExecuteTemplate(w, "index.gohtml", d)
-	if err3 != nil {
-		log.Println(err3)
+	handleErr(err3)
+}
+func handleErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
 	}
 }
