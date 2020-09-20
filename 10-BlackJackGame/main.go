12fc53feacda4e9ffa67ae3655031a9572a9418f
ARMIN <armingodarzi1380@gmail.com>
2020-09-20 20:41:54 +0430

    adding play and scoring funcs

--- 10-BlackJackGame/main.go
+++ 10-BlackJackGame/main.go
@@ -10,31 +10,71 @@ type Dealer struct {
 }
 type Player struct {
 	NickName string
-	Cards []deck.Card
+	Cards    []deck.Card
 }
 type User interface {
 	userTurn()
+	play()
 }
+
 var cards []deck.Card
+var holder = 0
+var dealer Dealer
 
 func main() {
-	cards = deck.NewDeck()
-	dealer := Dealer{[]deck.Card{}}
-	player := Player{"armin",[]deck.Card{}}
-	users:= []User{}
+	cards = deck.NewDeck(deck.MultipleDeck(2), deck.Shuffle)
+	fmt.Println(cards)
+	dealer = Dealer{[]deck.Card{}}
+	player := Player{"armin", []deck.Card{}}
+	users := []User{}
 	users = append(users, &dealer)
 	users = append(users, &player)
 	Start(users)
 }
 
 func Start(users []User) {
-	for _,user := range users{
+	for _, user := range users {
 		user.userTurn()
 	}
 }
 func (dealer *Dealer) userTurn() {
 	fmt.Println("DEALER TURN : ")
+	dealer.Cards = append(dealer.Cards, cards[holder], cards[holder+1])
+	holder += 2
+	fmt.Println(dealer)
 }
 func (player *Player) userTurn() {
-	fmt.Println("PLAYER "+ player.NickName+" TURN : ")
+	fmt.Println("PLAYER " + player.NickName + " TURN : ")
+	player.Cards = append(player.Cards, cards[holder], cards[holder+1])
+	holder += 2
+	fmt.Println(player)
+	fmt.Println("DEALER CARD IS : " + dealer.Cards[0].Rank.String() + " OF " + dealer.Cards[0].Suit.String() + "s")
+	player.play()
+}
+func (player *Player) play() {
+	fmt.Println("1 ) HIT \n2 ) STAND ")
+	var input int
+	fmt.Scan(&input)
+	switch input {
+	case 1:
+		player.Cards = append(player.Cards, cards[holder], cards[holder+1])
+		holder += 1
+		player.play()
+	case 2:
+		break
+	}
+}
+func (dealer *Dealer) play() {
+	fmt.Println("1 ) HIT \n2 ) STAND ")
+	var input int
+	fmt.Scan(&input)
+	switch input {
+	case 1:
+	case 2:
+		break
+	}
+}
+func scoring(cards []deck.Card) int {
+	var score = 0
+	return score
 }
