e8bf6bf4099b96298702ccb968d5a056c46ab045
ARMIN <armingodarzi1380@gmail.com>
2020-09-20 20:55:23 +0430

    userTurn for player done

--- 10-BlackJackGame/main.go
+++ 10-BlackJackGame/main.go
@@ -14,7 +14,7 @@ type Player struct {
 }
 type User interface {
 	userTurn()
-	play()
+	play() int
 }
 
 var cards []deck.Card
@@ -25,9 +25,10 @@ func main() {
 	cards = deck.NewDeck(deck.MultipleDeck(2), deck.Shuffle)
 	fmt.Println(cards)
 	dealer = Dealer{[]deck.Card{}}
+	dealer.Cards = append(dealer.Cards, cards[holder], cards[holder+1])
+	holder += 2
 	player := Player{"armin", []deck.Card{}}
 	users := []User{}
-	users = append(users, &dealer)
 	users = append(users, &player)
 	Start(users)
 }
@@ -36,11 +37,10 @@ func Start(users []User) {
 	for _, user := range users {
 		user.userTurn()
 	}
+	dealer.userTurn()
 }
 func (dealer *Dealer) userTurn() {
 	fmt.Println("DEALER TURN : ")
-	dealer.Cards = append(dealer.Cards, cards[holder], cards[holder+1])
-	holder += 2
 	fmt.Println(dealer)
 }
 func (player *Player) userTurn() {
@@ -51,7 +51,7 @@ func (player *Player) userTurn() {
 	fmt.Println("DEALER CARD IS : " + dealer.Cards[0].Rank.String() + " OF " + dealer.Cards[0].Suit.String() + "s")
 	player.play()
 }
-func (player *Player) play() {
+func (player *Player) play() int { // 0 for stand , 1 for win , -1  for lose
 	fmt.Println("1 ) HIT \n2 ) STAND ")
 	var input int
 	fmt.Scan(&input)
@@ -59,12 +59,23 @@ func (player *Player) play() {
 	case 1:
 		player.Cards = append(player.Cards, cards[holder], cards[holder+1])
 		holder += 1
-		player.play()
+		score, _ := scoring(player.Cards)
+		if score == 21 {
+			return 1
+		} else if score > 21 {
+			return -1
+		} else {
+			player.play()
+		}
 	case 2:
 		break
+	default:
+		fmt.Println("INVALID INPUT !")
+		player.play()
 	}
+	return 0
 }
-func (dealer *Dealer) play() {
+func (dealer *Dealer) play() int {
 	fmt.Println("1 ) HIT \n2 ) STAND ")
 	var input int
 	fmt.Scan(&input)
@@ -73,8 +84,10 @@ func (dealer *Dealer) play() {
 	case 2:
 		break
 	}
+	return 0
 }
-func scoring(cards []deck.Card) int {
+func scoring(cards []deck.Card) (int, string) {
 	var score = 0
-	return score
+	var Type = "normal"
+	return score, Type
 }
