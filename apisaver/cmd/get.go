7650d412128550f6ff943287d705dd4b5b85c14a
ARMIN <armingodarzi1380@gmail.com>
2020-10-27 01:50:36 +0330

    working on cli,get and set commend are being complete ...

--- apisaver/cmd/get.go
+++ apisaver/cmd/get.go
@@ -17,12 +17,13 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("get called")
+
+		fmt.Println(key)
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(getCmd)
+	RootCmd.AddCommand(getCmd)
 
 	// Here you will define your flags and configuration settings.
 
--- apisaver/cmd/set.go
+++ apisaver/cmd/set.go
@@ -3,23 +3,26 @@ package cmd
 import (
 	"fmt"
 
-
 	"github.com/spf13/cobra"
 )
 var key string
 
-var Set = &cobra.Command{
+var setCmd = &cobra.Command{
 	Use:   "set",
 	Short: "this Command will save your api secret key in encrypted mode",
 	Run: func(cmd *cobra.Command, args []string) {
     //TODO completing set func
-		fmt.Println(args)
-    fmt.Println("ADDED")
-    fmt.Println(key)
+    fmt.Print("name of api secret : ")
+    fmt.Println(args[0])
+    fmt.Print("received api secret :")
+    fmt.Println(args[1])
+    fi()
 	},
 }
 
 func init() {
-	RootCmd.AddCommand(Set)
-  Set.PersistentFlags().StringVar(&key, "config", "", "config file (default is $HOME/.cobra.yaml)")
+	RootCmd.AddCommand(setCmd)
+}
+func fi(){
+  fmt.Println("fuck me")
 }
