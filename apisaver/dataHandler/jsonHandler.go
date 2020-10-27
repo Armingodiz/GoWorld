b42c7fe91061bd3b5c3965ec6edadeb819e7ae1d
ARMIN <armingodarzi1380@gmail.com>
2020-10-27 16:44:42 +0330

    set func done

--- apisaver/dataHandler/jsonHandler.go
+++ apisaver/dataHandler/jsonHandler.go
@@ -1,2 +1,48 @@
 package dataHandler
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+var ApiSecrets *Secrets
+
+type ApiSecret struct {
+	Name   string
+	Secret string
+}
+
+type Secrets struct {
+	Secrets []ApiSecret `json:"apiSecrets"`
+}
+
+func init() {
+	ApiSecrets = readApiSecrets()
+}
+func readApiSecrets() *Secrets {
+	jsonFile, err := os.Open("secrets.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened secrets.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+	// read our opened xmlFile as a byte array.
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	// we initialize our Users array
+	ApiSecrets2 := &Secrets{}
+	// we unmarshal our byteArray which contains our
+	// jsonFile's content into 'users' which we defined above
+	json.Unmarshal(byteValue, ApiSecrets2)
+	if ApiSecrets2.Secrets == nil {
+		fmt.Println("#################################################################33")
+		ApiSecrets2 = &Secrets{
+			Secrets: []ApiSecret{},
+		}
+	}
+	fmt.Println(ApiSecrets2.Secrets)
+	return ApiSecrets2
+}
--- apisaver/dataHandler/saver.go
+++ apisaver/dataHandler/saver.go
@@ -1,11 +1,34 @@
 package dataHandler
 
 import (
-	"fmt"
-	"github.com/Armingodiz/go-stuff/apisaver/encryptor"
+	//"github.com/Armingodiz/go-stuff/apisaver/encryptor"
+	"encoding/json"
+"fmt"
+"io/ioutil"
 )
 
-func SaveData(name , api , key string){
-	encrypted := encryptor.Encrypt("Hello Encrypt", key)
-	fmt.Printf("encrypted : %s\n", encrypted)
+func SaveData(name, api, key string) {
+//	encrypted := encryptor.Encrypt("Hello Encrypt", key)
+//	fmt.Printf("encrypted : %s\n", encrypted)
+
+	apiSecret := ApiSecret{
+		Name: name,
+		Secret:  api,
+	}
+	fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1")
+	fmt.Println(ApiSecrets.Secrets)
+	fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1")
+	ApiSecrets.Secrets = append(ApiSecrets.Secrets, apiSecret)
+	fmt.Println(ApiSecrets.Secrets)
+	fmt.Println("11111111111111111111111111111")
+	data := Secrets{
+		Secrets: ApiSecrets.Secrets,
+	}
+	fmt.Println(data)
+	jsonString, _ := json.Marshal(data)
+	err := ioutil.WriteFile("secrets.json", jsonString, 0644)
+	if err != nil {
+		fmt.Println("errrrrrrrrrrrrrrrrrrrr")
+	}
+	fmt.Println("yessssssssssssssss")
 }
