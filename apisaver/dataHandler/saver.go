d62411d978fcb1119e4213437344d3502e2387f3
ARMIN <armingodarzi1380@gmail.com>
2020-10-27 16:50:41 +0330

    updates in saveData func

--- apisaver/dataHandler/saver.go
+++ apisaver/dataHandler/saver.go
@@ -1,34 +1,26 @@
 package dataHandler
 
 import (
-	//"github.com/Armingodiz/go-stuff/apisaver/encryptor"
 	"encoding/json"
-"fmt"
-"io/ioutil"
+	"fmt"
+	"github.com/Armingodiz/go-stuff/apisaver/encryptor"
+	"io/ioutil"
 )
 
 func SaveData(name, api, key string) {
-//	encrypted := encryptor.Encrypt("Hello Encrypt", key)
-//	fmt.Printf("encrypted : %s\n", encrypted)
-
+	encrypted := encryptor.Encrypt(api, key)
+	//fmt.Printf("encrypted : %s\n", encrypted)
 	apiSecret := ApiSecret{
-		Name: name,
-		Secret:  api,
+		Name:   name,
+		Secret: encrypted,
 	}
-	fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1")
-	fmt.Println(ApiSecrets.Secrets)
-	fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1")
 	ApiSecrets.Secrets = append(ApiSecrets.Secrets, apiSecret)
-	fmt.Println(ApiSecrets.Secrets)
-	fmt.Println("11111111111111111111111111111")
 	data := Secrets{
 		Secrets: ApiSecrets.Secrets,
 	}
-	fmt.Println(data)
 	jsonString, _ := json.Marshal(data)
 	err := ioutil.WriteFile("secrets.json", jsonString, 0644)
 	if err != nil {
-		fmt.Println("errrrrrrrrrrrrrrrrrrrr")
+		fmt.Println("error in writing on json !")
 	}
-	fmt.Println("yessssssssssssssss")
 }
