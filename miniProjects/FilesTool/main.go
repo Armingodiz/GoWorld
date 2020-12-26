a7f347f9f1e21e69071530f351946035f5ea4e8a
ARMIN <armingodarzi1380@gmail.com>
2020-12-26 22:41:48 +0330

    find method added for tool & tested

--- miniProjects/FilesTool/main.go
+++ miniProjects/FilesTool/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
-  "os"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+  "strings"
 )
 
 type Performer interface {
@@ -22,18 +26,35 @@ func (t *Tool) SetPerformer(performer Performer) {
 	t.performer = performer
 }
 
-
-
 func (t *Tool) Performe(chosen []os.FileInfo) error {
 	return t.performer.Task(chosen)
 }
 
 func main() {
 	tool := NewTool(nil)
-  tool.SetPerformer(NewCategorise(true))
-  tool.Performe(nil)
-  tool.SetPerformer(NewRename("",true,""))
-  tool.Performe(nil)
-  tool.SetPerformer(NewTransfer(""))
-  tool.Performe(nil)
+	chosen := tool.Find("./sample", "test")
+  fmt.Println(chosen[0].Name())
+	tool.SetPerformer(NewCategorise(true))
+	tool.Performe(nil)
+	tool.SetPerformer(NewRename("", true, ""))
+	tool.Performe(nil)
+	tool.SetPerformer(NewTransfer(""))
+	tool.Performe(nil)
+}
+
+func (t *Tool) Find(dir, pattern string) []os.FileInfo {
+	files, err := ioutil.ReadDir(dir)
+  chosen := []os.FileInfo{}
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("###########found files in  dir  : ")
+	for _, file := range files {
+		fmt.Print(file.Name())
+    if strings.Contains(file.Name(),pattern){
+      chosen = append(chosen, file)
+    }
+	}
+	fmt.Println("######################################")
+	return chosen
 }
