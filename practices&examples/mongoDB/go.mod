479c781b9d6484865c9b33e498f5a807ce4c5aad
ArminGodiz <armingodarzi1380@gmail.com>
2021-03-04 17:35:21 +0330

    CreateUser function added

--- practices&examples/mongoDB/go.mod
+++ practices&examples/mongoDB/go.mod
@@ -2,4 +2,7 @@ module gomongo
 
 go 1.13
 
-require go.mongodb.org/mongo-driver v1.4.6 // indirect
+require (
+	github.com/fatih/color v1.10.0 // indirect
+	go.mongodb.org/mongo-driver v1.4.6 // indirect
+)
---
+++ practices&examples/mongoDB/users_db.go
@@ -0,0 +1,222 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+//User - struct to map with mongodb documents
+type User struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	CreatedAt time.Time          `bson:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at"`
+	Name      string             `bson:"naem"`
+	Email     string             `bson:"email"`
+	Password  string             `bson:"password"`
+}
+
+//CreateIssue - Insert a new document in the collection.
+func CreateUser(user User) error {
+	client, err := GetMongoClient()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	//Create a handle to the respective collection in the database.
+	// client.Database() create a DB if the db doesnt exists.
+	collection := client.Database(DB).Collection(Users)
+	_, err = collection.InsertOne(context.TODO(), user)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+//PrintList - Print list of users on console
+func PrintList(issues []User) {
+	for i, u := range issues {
+		fmt.Printf("%d: %s    %s\n", i+1, u.Name, u.Email)
+	}
+}
+
+//GetAllIssues - Get All issues for collection
+func GetAllUsers() ([]User, error) {
+	//Define filter query for fetching specific document from collection
+	filter := bson.D{{}} //bson.D{{}} specifies 'all documents'
+	var users []User
+	//Get MongoDB connection using connectionhelper.
+	client, err := GetMongoClient()
+	if err != nil {
+		return users, err
+	}
+	//Create a handle to the respective collection in the database.
+	collection := client.Database(DB).Collection(Users)
+	//Perform Find operation & validate against the error.
+	cur, findError := collection.Find(context.TODO(), filter)
+	if findError != nil {
+		return users, findError
+	}
+	//Map result to slice
+	for cur.Next(context.TODO()) {
+		var u User
+		err := cur.Decode(&u)
+		if err != nil {
+			return users, err
+		}
+		users = append(users, u)
+	}
+	// once exhausted, close the cursor
+	cur.Close(context.TODO())
+	if len(users) == 0 {
+		return users, mongo.ErrNoDocuments
+	}
+	return users, nil
+}
+
+/*
+//CreateMany - Insert multiple documents at once in the collection.
+func CreateMany(list []Issue) error {
+	//Map struct slice to interface slice as InsertMany accepts interface slice as parameter
+	insertableList := make([]interface{}, len(list))
+	for i, v := range list {
+		insertableList[i] = v
+	}
+	//Get MongoDB connection using connectionhelper.
+	client, err := connectionhelper.GetMongoClient()
+	if err != nil {
+		return err
+	}
+	//Create a handle to the respective collection in the database.
+	collection := client.Database(connectionhelper.DB).Collection(connectionhelper.ISSUES)
+	//Perform InsertMany operation & validate against the error.
+	_, err = collection.InsertMany(context.TODO(), insertableList)
+	if err != nil {
+		return err
+	}
+	//Return success without any error.
+	return nil
+}
+
+//GetIssuesByCode - Get All issues for collection
+func GetIssuesByCode(code string) (Issue, error) {
+	result := Issue{}
+	//Define filter query for fetching specific document from collection
+	filter := bson.D{primitive.E{Key: "code", Value: code}}
+	//Get MongoDB connection using connectionhelper.
+	client, err := connectionhelper.GetMongoClient()
+	if err != nil {
+		return result, err
+	}
+	//Create a handle to the respective collection in the database.
+	collection := client.Database(connectionhelper.DB).Collection(connectionhelper.ISSUES)
+	//Perform FindOne operation & validate against the error.
+	err = collection.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		return result, err
+	}
+	//Return result without any error.
+	return result, nil
+}
+
+//GetAllIssues - Get All issues for collection
+func GetAllIssues() ([]Issue, error) {
+	//Define filter query for fetching specific document from collection
+	filter := bson.D{{}} //bson.D{{}} specifies 'all documents'
+	var issues []Issue
+	//Get MongoDB connection using connectionhelper.
+	client, err := connectionhelper.GetMongoClient()
+	if err != nil {
+		return issues, err
+	}
+	//Create a handle to the respective collection in the database.
+	collection := client.Database(connectionhelper.DB).Collection(connectionhelper.ISSUES)
+	//Perform Find operation & validate against the error.
+	cur, findError := collection.Find(context.TODO(), filter)
+	if findError != nil {
+		return issues, findError
+	}
+	//Map result to slice
+	for cur.Next(context.TODO()) {
+		var t Issue
+		err := cur.Decode(&t)
+		if err != nil {
+			return issues, err
+		}
+		issues = append(issues, t)
+	}
+	// once exhausted, close the cursor
+	cur.Close(context.TODO())
+	if len(issues) == 0 {
+		return issues, mongo.ErrNoDocuments
+	}
+	return issues, nil
+}
+
+//DeleteOne - Get All issues for collection
+func DeleteOne(code string) error {
+	//Define filter query for fetching specific document from collection
+	filter := bson.D{primitive.E{Key: "code", Value: code}}
+	//Get MongoDB connection using connectionhelper.
+	client, err := connectionhelper.GetMongoClient()
+	if err != nil {
+		return err
+	}
+	//Create a handle to the respective collection in the database.
+	collection := client.Database(connectionhelper.DB).Collection(connectionhelper.ISSUES)
+
+	//Perform DeleteOne operation & validate against the error.
+	_, err = collection.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		return err
+	}
+	//Return success without any error.
+	return nil
+}
+
+//DeleteAll - Get All issues for collection
+func DeleteAll() error {
+	//Define filter query for fetching specific document from collection
+	selector := bson.D{{}} // bson.D{{}} specifies 'all documents'
+	//Get MongoDB connection using connectionhelper.
+	client, err := connectionhelper.GetMongoClient()
+	if err != nil {
+		return err
+	}
+	//Create a handle to the respective collection in the database.
+	collection := client.Database(connectionhelper.DB).Collection(connectionhelper.ISSUES)
+	//Perform DeleteMany operation & validate against the error.
+	_, err = collection.DeleteMany(context.TODO(), selector)
+	if err != nil {
+		return err
+	}
+	//Return success without any error.
+	return nil
+}
+
+
+
+*/
+func main() {
+	/*user := User{
+		ID:        primitive.NewObjectID(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      "armin",
+		Email:     "armin@gmail.com",
+		Password:  "3011",
+	}*/
+	//CreateUser(user)
+	// DeleteOne("I0001")
+	//DeleteAll()
+	users, err := GetAllUsers()
+	if err != nil {
+		fmt.print(err)
+	}
+	PrintList(users)
+}
